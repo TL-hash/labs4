6f198227226746aa6328d40cfde236b7ce9a9b60
TL-hash <terem158@mail.ru>
2025-10-26 16:11:39 +0300

    Adding task 3

---
+++ task3.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+if __name__ == "__main__":
+    A = tuple(map(int, input().split()))
+    if len(A) < 2:
+        print("Кортеж должен содержать хотя бы 2 элемента", file=sys.stderr)
+        exit(1)
+
+    found_pair = False
+    last_pos = -1
+
+    for i in range(len(A) - 1):
+        if A[i] % 2 == 0 and A[i + 1] % 2 == 0:
+            found_pair = True
+            last_pos = i
+            print(f"Найдена пара четных: ({A[i]}, {A[i + 1]}) на позициях {i} и {i + 1}")
+
+    if found_pair:
+        print("Элементы перед последней парой четных:")
+        for i in range(last_pos):
+            print(A[i], end=" ")
+    else:
+        print("В кортеже нет пар соседних четных элементов")
