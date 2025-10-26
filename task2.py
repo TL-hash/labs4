8c37bd67ba5767582bf014b03fd3865eff5f54d9
TL-hash <terem158@mail.ru>
2025-10-26 16:03:11 +0300

    Adding task 2

---
+++ task2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+
+if __name__ == '__main__':
+    A = list(map(float, input().split()))
+    if not A:
+        print("Заданный список пуст", file=sys.stderr)
+        exit(1)
+
+    i_min = A.index(min(A))
+    print(f"Номер минимального элемента: {i_min}")
+
+    i_neg = [i for i, item in enumerate(A) if item < 0]
+
+    if len(i_neg) < 2:
+        sum_between = 0
+        print(f"Недостаточно отрицательных элементов для вычисления суммы")
+    else:
+        first_neg = i_neg[0]
+        second_neg = i_neg[1]
+        sum_between = sum(A[first_neg+1:second_neg])
+        print(f"Сумма между первым и вторым отрицательными элементами: {sum_between}")
+
+    low_abs = [item for item in A if abs(item) <= 1]
+    high_abs = [item for item in A if abs(item) > 1]
+    sorted_list = low_abs + high_abs
+
+    print(f"Преобразованный список: {sorted_list}")
