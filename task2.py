4e82cf1df46f9730a96824e56042f3c312f4cc67
TL-hash <terem158@mail.ru>
2025-11-11 22:42:13 +0300

    Edit task 2

--- task2.py
+++ task2.py
@@ -5,15 +5,15 @@ import sys
 
 
 if __name__ == '__main__':
-    A = list(map(float, input().split()))
-    if not A:
+    a = list(map(float, input().split()))
+    if not a:
         print("Заданный список пуст", file=sys.stderr)
         exit(1)
 
-    i_min = A.index(min(A))
+    i_min = a.index(min(a))
     print(f"Номер минимального элемента: {i_min}")
 
-    i_neg = [i for i, item in enumerate(A) if item < 0]
+    i_neg = [i for i, item in enumerate(a) if item < 0]
 
     if len(i_neg) < 2:
         sum_between = 0
@@ -21,11 +21,11 @@ if __name__ == '__main__':
     else:
         first_neg = i_neg[0]
         second_neg = i_neg[1]
-        sum_between = sum(A[first_neg+1:second_neg])
+        sum_between = sum(a[first_neg+1:second_neg])
         print(f"Сумма между первым и вторым отрицательными элементами: {sum_between}")
 
-    low_abs = [item for item in A if abs(item) <= 1]
-    high_abs = [item for item in A if abs(item) > 1]
+    low_abs = [item for item in a if abs(item) <= 1]
+    high_abs = [item for item in a if abs(item) > 1]
     sorted_list = low_abs + high_abs
     #A.sort(key=lambda x: abs(x) > 1)
 
