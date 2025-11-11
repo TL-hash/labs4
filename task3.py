587f3a66d324db07c25bf4bd540b96475fb38e51
TL-hash <terem158@mail.ru>
2025-11-11 22:51:58 +0300

    Edit task 3

--- task3.py
+++ task3.py
@@ -4,22 +4,20 @@
 import sys
 
 if __name__ == "__main__":
-    A = tuple(map(int, input().split()))
-    if len(A) < 2:
+    a = tuple(map(int, input().split()))
+    if len(a) < 2:
         print("Кортеж должен содержать хотя бы 2 элемента", file=sys.stderr)
         exit(1)
 
-    found_pair = False
     last_pos = -1
 
-    for i in range(len(A) - 1):
-        if A[i] % 2 == 0 and A[i + 1] % 2 == 0:
-            found_pair = True
+    for i, (x, y) in enumerate(zip(a, a[1:])):
+        if x % 2 == 0 and y % 2 == 0:
             last_pos = i
 
-    if found_pair:
-        print("Элементы перед последней парой четных:")
-        for i in range(last_pos):
-            print(A[i], end=" ")
-    else:
+    if last_pos == -1:
         print("В кортеже нет пар соседних четных элементов")
+    else:
+        print("Элементы перед последней парой четных:")
+        for elem in a[:last_pos]:
+            print(elem, end=" ")
