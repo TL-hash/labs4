884304aa77bed6b89cb9bdd7d1643a0f1276424c
TL-hash <terem158@mail.ru>
2025-12-09 23:46:21 +0300

    Editing task 2

--- Tasks/task2.py
+++ Tasks/task2.py
@@ -3,31 +3,37 @@
 
 import sys
 
-
 if __name__ == '__main__':
     a = list(map(float, input().split()))
     if not a:
         print("Заданный список пуст", file=sys.stderr)
         exit(1)
 
-    i_min = a.index(min(a))
-    print(f"Номер минимального элемента: {i_min}")
+    min_val = a[0]
+    min_index = 0
+    first_neg_index = 0
+    second_neg_index = 0
+    sum_between = 0
 
-    i_neg = [i for i, item in enumerate(a) if item < 0]
+    for i, value in enumerate(a):
+        if value < min_val:
+            min_val = value
+            min_index = i
 
-    if len(i_neg) < 2:
-        sum_between = 0
-        print(f"Недостаточно отрицательных элементов для вычисления суммы")
-    else:
-        first_neg = i_neg[0]
-        second_neg = i_neg[1]
-        sum_between = sum(a[first_neg+1:second_neg])
+        if value < 0:
+            if first_neg_index == 0:
+                first_neg_index = i
+            elif second_neg_index == 0:
+                second_neg_index = i
+
+    print(f"Номер минимального элемента: {min_index}")
+
+    if first_neg_index != 0 and second_neg_index != 0:
+        sum_between = sum(a[first_neg_index + 1:second_neg_index])
         print(f"Сумма между первым и вторым отрицательными элементами: {sum_between}")
+    else:
+        print("Недостаточно отрицательных элементов для вычисления суммы")
 
-    low_abs = [item for item in a if abs(item) <= 1]
-    high_abs = [item for item in a if abs(item) > 1]
-    sorted_list = low_abs + high_abs
-    #A.sort(key=lambda x: abs(x) > 1)
+    a.sort(key=lambda x: abs(x) > 1)
 
-    print(f"Преобразованный список: {sorted_list}")
-    #print(f"Преобразованный список: {A}")
+    print(f"Преобразованный список: {a}")
